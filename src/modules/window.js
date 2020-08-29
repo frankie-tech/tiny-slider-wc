3eeab902f111eb1556372a84697fed6a09a0bd62
frankie <26461046+frankiekie@users.noreply.github.com>
2020-08-28 20:23:38 -0400

    creates window utils
    mimics the window utils functions that are used inside of
    tiny-slider originally
    
    Not sure if this is the right approach, but I think it can become useful

---
+++ src/modules/window.js
@@ -0,0 +1,40 @@
+export default class WindowUtil {
+	constructor(options) {
+		this.edgePadding = options.edgePadding || false;
+		this.containerParent = options.containerParent;
+	}
+
+	// equal to https://github.com/ganlanyuan/tiny-slider/blob/master/src/tiny-slider.js#L533-L535
+	static get windowWidth() {
+		return (
+			window.innerWidth ||
+			document.documentElement.clientWidth ||
+			document.body.clientWidth
+		);
+	}
+
+	// equal to https://github.com/ganlanyuan/tiny-slider/blob/master/src/tiny-slider.js#L541-L549
+	static set setClientWidth(el) {
+		if (el == null) return;
+		const div = document.createElement('div');
+		el.appendChild(div);
+		const { right, left } = div.getBoundingClientRect();
+		const width = right - left;
+
+		div.remove();
+
+		if (width) {
+			this.clientWidth = width;
+		} else {
+			this.setClientWidth = el.parentNode;
+		}
+		// this.clientWidth = width || this.setClientWidth(el.parentNode);
+	}
+
+	// equal to https://github.com/ganlanyuan/tiny-slider/blob/master/src/tiny-slider.js#L551-L554
+	static get viewportWidth() {
+		var gap = this.edgePadding ? this.edgePadding * 2 - this.gutter : 0;
+		this.setClientWidth = this.containerParent;
+		return this.clientWidth - gap;
+	}
+}
